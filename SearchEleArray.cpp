dbf267b58853ac4786b8c9b7d330bea16b229cc9
Sumit tripathi <sumittripathi8795@gmail.in>
2025-07-31 23:40:16 +0530

    element search in array

--- SearchEleArray.cpp
+++ SearchEleArray.cpp
@@ -1,5 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    return 0;
+    int arr[7] = {30, 60, 5, 36, 59, 45, 1};
+    int i, target = 1;
+    for (i = 0; i < 7; i++ ){
+        if(arr[i] == target){
+            cout << "This is " << arr[i];
+        }
+    }
+        return 0;
 }
