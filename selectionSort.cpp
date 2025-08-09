6fb80667a227726f324b536da89d3c493abff85a
Sumit tripathi <sumittripathi8795@gmail.in>
2025-08-09 12:44:53 +0530

    Bubble Sort  basic to advance level

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -20,3 +20,31 @@ int main(){
     
     return 0;
 }
+
+
+
+// Using Selection Sort to sort the given array into descending order:
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5] = {78, 734, 978, 3, 1};
+    for (int i = 0; i < 4; i++)
+    {
+        int index = i;
+        for (int j = i + 1; j < 5; j++)
+        {
+            if (arr[j] > arr[index])
+                index = j;
+        }
+        swap(arr[i], arr[index]);
+    }
+    cout << "Sorted array in descending order: ";
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
